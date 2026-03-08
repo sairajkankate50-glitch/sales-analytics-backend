7b47e761ff284b816b6dbe5a66f9b302fdef4c13
sairaj kankate <chandrakanktkankate09@gamil.com>
2026-03-08 17:41:16 +0530

    fixed duplicate root route

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
-import express from 'express'
-import cors from 'cors';
-import dotenv from 'dotenv';
-import connectDB from './config/db.js';
-import analyticsRoutes from "./router/route.js"
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import analyticsRoutes from "./router/route.js";
 
-dotenv.config()
+dotenv.config();
+
 console.log("calling DB connection...");
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
+
+// API routes
 app.use("/api/analytics", analyticsRoutes);
-app.get("/",(req,res)=>{
-    res.send("api is running")
-})
-app.get("/", (req, res) => {
-  res.send("API is running 🚀");
-});
-app.listen(process.env.PORT,()=>{
-    console.log(`server is running on port ${process.env.PORT}`)
-})
 
+// Root route
+app.get("/", (req, res) => {
+  res.send("Sales Analytics API is running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
